9609a25ab0348c6938c538f11999d1130c8daf60
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-04-02 10:16:37 +0530

    Babel Setup

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,13 @@ module.exports = {
   },
   devServer: {
     static: {
-        directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3000,
     open: true,
     hot: true,
     compress: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -24,13 +24,23 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack App",
       filename: "index.html",
-      template: './src/index.html'
+      template: "./src/index.html",
     }),
   ],
 };
